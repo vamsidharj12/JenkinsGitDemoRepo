a4cca57b18d8e1270a5dad8b1447d3adafe17643
Vamsidhar Julakanti <vamsidhar.julakanti@fourcolorstech.com>
2023-12-19 15:56:09 -0600

    additional test cases

--- src/main/java/Automation/AWCTeamCenter/JenkinsGitDemoTest.java
+++ src/main/java/Automation/AWCTeamCenter/JenkinsGitDemoTest.java
@@ -31,5 +31,82 @@ package Automation.AWCTeamCenter;
 		assertEquals(title,"Swag Labs");
 		driver.quit();
 
-	}
+	    }
+		
+		public void test2 () throws InterruptedException {
+			
+			//WebDriverManager.chromedriver().setup();
+			WebDriver driver = new ChromeDriver();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			driver.get("https://www.saucedemo.com/");
+			Thread.sleep(5000);
+			driver.findElement(By.xpath("//div/input[@id='user-name']")).sendKeys("standard_user");
+			driver.findElement(By.xpath("//div/input[@id='password']")).sendKeys("secret_sauce");
+			driver.findElement(By.xpath("//input[@id='login-button']")).click();
+			System.out.print("Logged into the application");
+			Thread.sleep(5000);
+			String title = driver.getTitle();
+			assertEquals(title,"Swag Labs");
+			System.out.print("Test 2 is completed");
+			driver.quit();
+
+		    }
+	public void test3 () throws InterruptedException {
+			
+			//WebDriverManager.chromedriver().setup();
+			WebDriver driver = new ChromeDriver();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			driver.get("https://www.saucedemo.com/");
+			Thread.sleep(5000);
+			driver.findElement(By.xpath("//div/input[@id='user-name']")).sendKeys("standard_user");
+			driver.findElement(By.xpath("//div/input[@id='password']")).sendKeys("secret_sauce");
+			driver.findElement(By.xpath("//input[@id='login-button']")).click();
+			System.out.print("Logged into the application");
+			Thread.sleep(5000);
+			String title = driver.getTitle();
+			assertEquals(title,"Swag Labs");
+			System.out.print("Test 3 is completed");
+			driver.quit();
+
+		    }
+	
+	public void test4 () throws InterruptedException {
+		
+		//WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.saucedemo.com/");
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//div/input[@id='user-name']")).sendKeys("standard_user");
+		driver.findElement(By.xpath("//div/input[@id='password']")).sendKeys("secret_sauce");
+		driver.findElement(By.xpath("//input[@id='login-button']")).click();
+		System.out.print("Logged into the application");
+		Thread.sleep(5000);
+		String title = driver.getTitle();
+		assertEquals(title,"Swag Labs");
+		System.out.print("Test 4 is completed");
+		driver.quit();
+
+	    }
+	
+	public void test5 () throws InterruptedException {
+		
+		//WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.saucedemo.com/");
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//div/input[@id='user-name']")).sendKeys("standard_user");
+		driver.findElement(By.xpath("//div/input[@id='password']")).sendKeys("secret_sauce");
+		driver.findElement(By.xpath("//input[@id='login-button']")).click();
+		System.out.print("Logged into the application");
+		Thread.sleep(5000);
+		String title = driver.getTitle();
+		assertEquals(title,"Swag Labs");
+		System.out.print("Test 5 is completed");
+		driver.quit();
+
+	    }
+
+
 	}
