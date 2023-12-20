21d7168ff9a3c0a9339f51a2285cc977aca4c40c
Vamsidhar Julakanti <vamsidhar.julakanti@fourcolorstech.com>
2023-12-20 11:50:22 -0600

    updates for docker

--- src/main/java/Automation/AWCTeamCenter/JenkinsGitDemoTest.java
+++ src/main/java/Automation/AWCTeamCenter/JenkinsGitDemoTest.java
@@ -5,105 +5,139 @@ package Automation.AWCTeamCenter;
 
 	import static org.testng.Assert.assertEquals;
 
-	import java.time.Duration;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
 
 	import org.openqa.selenium.By;
 	import org.openqa.selenium.WebDriver;
 	import org.openqa.selenium.chrome.ChromeDriver;
-	import org.testng.annotations.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
 
 	@Test
 	public class JenkinsGitDemoTest {
 		
-		public void test1 () throws InterruptedException {
+		public void test1 () throws InterruptedException, MalformedURLException {
 			
 		//WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		//WebDriver driver = new ChromeDriver();
+		//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			DesiredCapabilities dc= new DesiredCapabilities();
+			dc.setBrowserName("chrome");
+			URL url = new URL("http://localhost:4444/wd/hub");
+			RemoteWebDriver driver = new RemoteWebDriver(url,dc);	
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));	
 		driver.get("https://www.saucedemo.com/");
 		Thread.sleep(5000);
 		driver.findElement(By.xpath("//div/input[@id='user-name']")).sendKeys("standard_user");
 		driver.findElement(By.xpath("//div/input[@id='password']")).sendKeys("secret_sauce");
 		driver.findElement(By.xpath("//input[@id='login-button']")).click();
-		System.out.print("Logged into the application");
+		System.out.println("Logged into the application");
 		Thread.sleep(5000);
 		String title = driver.getTitle();
 		assertEquals(title,"Swag Labs");
+		System.out.println("Test 1 is completed");
 		driver.quit();
 
 	    }
 		
-		public void test2 () throws InterruptedException {
+		public void test2 () throws InterruptedException,MalformedURLException {
 			
 			//WebDriverManager.chromedriver().setup();
-			WebDriver driver = new ChromeDriver();
+			//WebDriver driver = new ChromeDriver();
+			//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			
+			DesiredCapabilities dc= new DesiredCapabilities();
+			dc.setBrowserName("chrome");
+			URL url = new URL("http://localhost:4444/wd/hub");
+			RemoteWebDriver driver = new RemoteWebDriver(url,dc);
 			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 			driver.get("https://www.saucedemo.com/");
 			Thread.sleep(5000);
 			driver.findElement(By.xpath("//div/input[@id='user-name']")).sendKeys("standard_user");
 			driver.findElement(By.xpath("//div/input[@id='password']")).sendKeys("secret_sauce");
 			driver.findElement(By.xpath("//input[@id='login-button']")).click();
-			System.out.print("Logged into the application");
+			System.out.println("Logged into the application");
 			Thread.sleep(5000);
 			String title = driver.getTitle();
 			assertEquals(title,"Swag Labs");
-			System.out.print("Test 2 is completed");
+			System.out.println("Test 2 is completed");
 			driver.quit();
 
 		    }
-	public void test3 () throws InterruptedException {
-			
-			//WebDriverManager.chromedriver().setup();
-			WebDriver driver = new ChromeDriver();
-			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	public void test3 () throws InterruptedException ,MalformedURLException {
+		
+		//WebDriverManager.chromedriver().setup();
+		//WebDriver driver = new ChromeDriver();
+		//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		DesiredCapabilities dc= new DesiredCapabilities();
+		dc.setBrowserName("chrome");
+		URL url = new URL("http://localhost:4444/wd/hub");
+		RemoteWebDriver driver = new RemoteWebDriver(url,dc);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 			driver.get("https://www.saucedemo.com/");
 			Thread.sleep(5000);
 			driver.findElement(By.xpath("//div/input[@id='user-name']")).sendKeys("standard_user");
 			driver.findElement(By.xpath("//div/input[@id='password']")).sendKeys("secret_sauce");
 			driver.findElement(By.xpath("//input[@id='login-button']")).click();
-			System.out.print("Logged into the application");
+			System.out.println("Logged into the application\n");
 			Thread.sleep(5000);
 			String title = driver.getTitle();
 			assertEquals(title,"Swag Labs");
-			System.out.print("Test 3 is completed");
+			System.out.println("Test 3 is completed");
 			driver.quit();
 
 		    }
 	
-	public void test4 () throws InterruptedException {
+	public void test4 () throws InterruptedException ,MalformedURLException {
 		
 		//WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
+		//WebDriver driver = new ChromeDriver();
+		//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		DesiredCapabilities dc= new DesiredCapabilities();
+		dc.setBrowserName("chrome");
+		URL url = new URL("http://localhost:4444/wd/hub");
+		RemoteWebDriver driver = new RemoteWebDriver(url,dc);	
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://www.saucedemo.com/");
 		Thread.sleep(5000);
 		driver.findElement(By.xpath("//div/input[@id='user-name']")).sendKeys("standard_user");
 		driver.findElement(By.xpath("//div/input[@id='password']")).sendKeys("secret_sauce");
 		driver.findElement(By.xpath("//input[@id='login-button']")).click();
-		System.out.print("Logged into the application");
+		System.out.println("Logged into the application");
 		Thread.sleep(5000);
 		String title = driver.getTitle();
 		assertEquals(title,"Swag Labs");
-		System.out.print("Test 4 is completed");
+		System.out.println("Test 4 is completed");
 		driver.quit();
 
 	    }
 	
-	public void test5 () throws InterruptedException {
+	public void test5 () throws InterruptedException ,MalformedURLException {
 		
 		//WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
+		//WebDriver driver = new ChromeDriver();
+		//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		DesiredCapabilities dc= new DesiredCapabilities();
+		dc.setBrowserName("chrome");
+		URL url = new URL("http://localhost:4444/wd/hub");
+		RemoteWebDriver driver = new RemoteWebDriver(url,dc);	
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://www.saucedemo.com/");
 		Thread.sleep(5000);
 		driver.findElement(By.xpath("//div/input[@id='user-name']")).sendKeys("standard_user");
 		driver.findElement(By.xpath("//div/input[@id='password']")).sendKeys("secret_sauce");
 		driver.findElement(By.xpath("//input[@id='login-button']")).click();
-		System.out.print("Logged into the application");
+		System.out.println("Logged into the application");
 		Thread.sleep(5000);
 		String title = driver.getTitle();
 		assertEquals(title,"Swag Labs");
-		System.out.print("Test 5 is completed");
+		System.out.println("Test 5 is completed");
 		driver.quit();
 
 	    }
